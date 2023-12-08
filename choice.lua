f2541e8e12d51d6fac85593fcacae263573395dc
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2023-12-07 22:16:51 -0300

    Update nodes!

--- choice.lua
+++ choice.lua
@@ -3,10 +3,11 @@ local class = require("libs.middleclass")
 ---@class Choice
 local Choice = class("Choice")
 
-function Choice:initialize(destination, description, condition)
+function Choice:initialize(destination, description, condition, routine)
     self.destination = destination ---@type string
     self.description = description ---@type string
-    self.condition = condition
+    self.condition = condition ---@type fun():boolean
+    self.routine = routine ---@type function
 end
 
 function Choice:hasCondition()
@@ -14,7 +15,16 @@ function Choice:hasCondition()
 end
 
 function Choice:runCondition()
+    if self.condition ~= nil and type(self.condition) == "function" then
+        return self.condition()
+    end
     return true
+end
+
+function Choice:runRoutine()
+    if self.routine ~= nil and type(self.routine) == "function" then
+        self.routine()
+    end
 end
 
 return Choice
--- main.lua
+++ main.lua
@@ -1,16 +1,21 @@
 local GameData = require("game_data")
 local Engine = require("engine")
 local nodeLoader = require("node_loader")
+local utils = require("utils")
 
 warn("@on")
 
-local gameData = GameData:new()
+utils.enableUtf8()
+
+local gameData = GameData:new() ---@type GameData
 _G.game = gameData
 
 nodeLoader.loadNodes()
+game.activeNode = nodeLoader.getInitialNode()
 
-for id, node in pairs (nodeLoader.getNodes()) do
-    print(id .. node.title)
+if nodeLoader.hasErrors() then
+    print("Found errors on NodeLoader, aborting execution!")
+    os.exit()
 end
 
 local engine = Engine:new() ---@type Engine
